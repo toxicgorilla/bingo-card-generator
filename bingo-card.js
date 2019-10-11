e624453ece6d5f96fd6506785f081bb3e2a37223
Stephen Hughes <steve.hughes@creative-technologies.co.uk>
2019-10-11 14:20:13 +0100

    Formatting. Added lorum ipsum strings

--- bingo-card.js
+++ bingo-card.js
@@ -1,32 +1,83 @@
 window.onload = initAll;
 var usedNums = new Array(76);
 
+var lines = [
+  "Phasellus gravida bibendum adipiscing.",
+  "Cras id rhoncus diam.",
+  "Praesent pharetra justo sed velit tempus, non pulvinar tortor tempor.",
+  "Maecenas elementum consequat orci, eget fermentum erat adipiscing porta.",
+  "Nunc et lorem eu metus pretium ultrices.",
+  "Nam pharetra a diam a accumsan.",
+  "Aliquam egestas ante eget enim condimentum lobortis.",
+  "Mauris euismod porta mollis.",
+  "Praesent eget ornare eros.",
+  "Nullam aliquet nulla eget erat congue, a sodales quam egestas.",
+  "Nunc urna purus, auctor non facilisis ac, viverra id lectus.",
+  "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+  "Curabitur rutrum hendrerit facilisis.",
+  "Pellentesque non scelerisque sapien.",
+  "Pellentesque quis purus egestas, mattis mauris vel, volutpat enim.",
+  "Maecenas eleifend sem in massa blandit eleifend.",
+  "Fusce nisi nulla, vehicula eleifend diam ac, rutrum facilisis diam.",
+  "Praesent eget leo a magna tincidunt ullamcorper quis eu massa.",
+  "Etiam ac tincidunt felis.",
+  "Aenean semper nisi sit amet nisl blandit, eget fermentum neque varius.",
+  "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+  "Nunc consequat, magna in gravida sollicitudin, nibh orci interdum tortor, vitae rutrum augue eros ut ante.",
+  "Nullam volutpat nisi sit amet justo pulvinar aliquam.",
+  "Nulla accumsan venenatis nisl id tempor."
+];
+
 function initAll() {
   if (document.getElementById) {
     document.getElementById("reload").onclick = anotherCard;
     newCard();
-  }
-  else{
+  } else {
     alert("Your browser does not support this script.");
   }
 }
 
 function newCard() {
-  for(var i=0 ; i<24 ; i++){
-      setSquare(i);
-    }
+  for (var i = 0; i < 24; i++) {
+    setSquare(i);
+  }
 }
 
-function setSquare(thisSquare){
+function setSquare(thisSquare) {
   var currentSquare = "square" + thisSquare;
-  var colPlace = new Array(0,1,2,3,4,0,1,2,3,4,0,1,3,4,0,1,2,3,4,0,1,2,3,4);
+  var colPlace = new Array(
+    0,
+    1,
+    2,
+    3,
+    4,
+    0,
+    1,
+    2,
+    3,
+    4,
+    0,
+    1,
+    3,
+    4,
+    0,
+    1,
+    2,
+    3,
+    4,
+    0,
+    1,
+    2,
+    3,
+    4
+  );
   var colBasis = colPlace[thisSquare] * 15;
   var newNum = colBasis + getNewNum() + 1;
 
-  do{
+  do {
     newNum = colBasis + getNewNum() + 1;
-  }while(usedNums[newNum]);
-  
+  } while (usedNums[newNum]);
+
   usedNums[newNum] = true;
   document.getElementById(currentSquare).innerHTML = newNum;
 }
@@ -38,7 +89,7 @@ function getNewNum() {
 function anotherCard() {
   for (var i = 1; i < usedNums.length; i++) {
     usedNums[i] = false;
-  };
+  }
 
   newCard();
   return false;
